a9f82e251dc1c4f1d51220d443ef9c590fae8396
patchipatch <vtruongg@ctemplar.com>
2022-04-27 19:25:27 -0400

    cleaning things

--- waybar/mediaplayer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-player_status=$(playerctl metadata -p spotify --format '{{status}}')
-if [ "$player_status" = "Playing" ]; then
-    echo " $(playerctl metadata --player=spotify --format '{{artist}} - {{title}}')"
-elif [ "$player_status" = "Paused" ]; then
-    echo " $(playerctl metadata --player=spotify --format '{{artist}} - {{title}}')"
-fi
