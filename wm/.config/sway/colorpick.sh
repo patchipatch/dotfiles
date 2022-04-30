04120ca9da744731257a363705397937dc99479a
patchipatch <vtruongg@ctemplar.com>
2022-04-30 11:14:30 -0400

    edits

--- wm/.config/sway/colorpick.sh
+++ wm/.config/sway/colorpick.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 coloroutput=$(grim -g "$(slurp -p)" -t ppm - | convert - -format '%[pixel:p{0,0}]' txt:- | tail --lines 1 | awk '{print "RGB: " $2 " Hex: " $3}')
-notify-send "Color in #RRGGBB" "$coloroutput"
+notify-send "Color in RGB/Hex" "$coloroutput"
